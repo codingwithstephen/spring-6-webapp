3023bb7bd48696a0642f7a15918a5edbe0a41d5d
devlegend <stephen.peter.mooney@gmail.com>
2023-05-16 14:21:35 +0100

    entities

---
+++ src/main/java/guru/springframework/spring6webapp/domain/Book.java
@@ -0,0 +1,24 @@
+package guru.springframework.spring6webapp.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String isbn;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
